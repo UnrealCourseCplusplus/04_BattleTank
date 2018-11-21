e05c8ec1d8e98bd7d907a5604829e08e338fa061
Abolfazl Karimi <abolfazl.karimi@gmail.com>
2018-11-21 15:35:41 -0500

    128. Re-parenting tank and tank-controller blueprints to new C++ classes

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -2,6 +2,22 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayterController not possessing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayterController possessing: %s"), *(ControlledTank->GetName()));
+	}
+	
+
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
